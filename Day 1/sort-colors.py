a1df6a81453e8fce7a06b1b49cc3a64d743125a3
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2022-06-01 23:00:43 +0530

    sort-colors

--- Day 1/sort-colors.py
+++ Day 1/sort-colors.py
@@ -1,8 +1,9 @@
 # ARRAYS - Easy
 
-# Given an array 'nums' with n objects colored red, white, or blue.
+# Given an array nums with n objects colored red, white, or blue.
 # Sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 # We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
 # You must solve this problem without using the library's sort function.
 
 class Solution:
