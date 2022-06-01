bc60ac3ab8da1eb209cde9fb9bb7b318ff04f7b8
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2022-06-01 23:00:10 +0530

    find-the-duplicate-number

--- Day 1/find-the-duplicate-number.py
+++ Day 1/find-the-duplicate-number.py
@@ -1,6 +1,6 @@
 # ARRAYS - Easy
 
-# Given an array of integers 'nums' containing n + 1 integers, where each integer is in the range [1, n] inclusive.
+# Given an array of integers nums containing n + 1 integers, where each integer is in the range [1, n] inclusive.
 # There is only one repeated number in nums, return this repeated number without modifying the array.
 
 class Solution:
