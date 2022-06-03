21d9e4516ea3e8fe6b9d461d4c5e921e2f83c69a
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2022-06-03 11:22:25 +0530

    two-sum

--- Day 2 - 02.06.22/two-sum.py
+++ Day 2 - 02.06.22/two-sum.py
@@ -1,1 +1,14 @@
+# ARRAYS - Easy
 
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            x = nums[i]
+            for j in range(i+1, len(nums)):
+                y = nums[j]
+                if x + y == target:
+                    return [i, j]
