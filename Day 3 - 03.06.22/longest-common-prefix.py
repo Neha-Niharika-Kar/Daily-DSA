89dc0b5348651bf73aa181be90e0c0c495003f57
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2022-06-03 14:54:08 +0530

    longest-common-prefix

--- Day 3 - 03.06.22/longest-common-prefix.py
+++ Day 3 - 03.06.22/longest-common-prefix.py
@@ -1,1 +1,8 @@
 # STRINGS - Easy
+
+# Find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        return os.path.commonprefix(strs)
