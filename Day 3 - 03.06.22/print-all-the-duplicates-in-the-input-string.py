58e4057503bddacd8717af83a05179ed866cf87f
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2022-06-03 14:43:16 +0530

    print-all-the-duplicates-in-the-input-string

--- Day 3 - 03.06.22/print-all-the-duplicates-in-the-input-string.py
+++ Day 3 - 03.06.22/print-all-the-duplicates-in-the-input-string.py
@@ -1,1 +1,15 @@
 # STRINGS - Easy
+
+# Given a string s, print all the duplicates and their counts in the string.
+
+class Solution:
+    def printDups(s):
+        char = {}
+        for i in range(len(s)):
+            if s[i] in char:
+                char[s[i]] += 1
+            else:
+                char[s[i]] = 1
+        for ch,i in char.items():  
+            if i > 1:  
+                print(ch, i)
