a8bedae805fc5946e045e7cc66d78782d0cdf1df
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2022-06-03 11:51:26 +0530

    valid-parentheses

--- Day 3 - 03.06.22/valid-parentheses.py
+++ Day 3 - 03.06.22/valid-parentheses.py
@@ -1,1 +1,23 @@
 # STRINGS - Easy
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if open brackets must be closed by the same type of brackets, and open brackets must be closed in the correct order.
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        l=[]
+        for i in range(len(s)):
+            if(s[i]=='{'or s[i]=="[" or s[i]=="("):
+                l.append(s[i])
+            elif(len(l)!=0 and l[-1]=="(" and s[i]==")"):
+                l.pop()
+            elif(len(l)!=0 and l[-1]=="[" and s[i]=="]"):
+                l.pop()
+            elif(len(l)!=0 and l[-1]=="{" and s[i]=="}"):
+                l.pop()
+            else:
+                return False
+        if(len(l) == 0):
+            return True
+        else:
+            return False
