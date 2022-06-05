1eda3c0aeed0358c8a59208772e619fdcc76e570
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2022-06-05 22:45:51 +0530

    excel-sheet-column-title

---
+++ Day 5 - 05.06.22/excel-sheet-column-title.py
@@ -0,0 +1,16 @@
+# MATHEMATICAL - Easy
+
+# Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        alphabets = {1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H',
+                     9:'I', 10:'J', 11:'K', 12:'L', 13:'M', 14: 'N', 15:'O', 
+                     16:'P', 17:'Q', 18:'R', 19:'S', 20:'T', 21:'U', 22:'V', 
+                     23:'W', 24:'X', 25:'Y', 26:'Z'}    
+        column = ''
+        while columnNumber:
+            rem = (columnNumber-1) % 26 + 1
+            column = alphabets[rem] + column
+            columnNumber = (columnNumber-1)//26
+        return column
