d56cb57270e6f5b38005bf6aa227f46a4473c461
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2022-06-07 19:50:01 +0530

    sort-a-linked-list-of-0s-1s-or-2s

---
+++ Day 7 - 07.06.22/sort-a-linked-list-of-0s-1s-or-2s.py
@@ -0,0 +1,27 @@
+# LINKED LIST - Easy
+
+# Given a linked list of 0s, 1s and 2s, sort it.
+# Return Type: head of the new list formed.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.data = x
+#         self.next = None
+class Solution:
+    def segregate(self, head):
+        count = [0, 0, 0]
+        ptr = head
+        while ptr != None:
+            count[ptr.data]+=1
+            ptr = ptr.next
+        i = 0
+        ptr = head
+        while ptr != None:
+            if count[i] == 0:
+                i+=1
+            else:
+                ptr.data = i
+                count[i]-=1
+                ptr = ptr.next
+        return head
