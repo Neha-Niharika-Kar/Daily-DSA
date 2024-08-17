a93a8ef07e141c0c03605c71c678a44d87b55812
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2024-08-17 21:50:10 +0530

    maximum-points-you-can-obtain-from-cards

--- Day 13 - 17.08.24/maximum-points-you-can-obtain-from-cards.py
+++ Day 13 - 17.08.24/maximum-points-you-can-obtain-from-cards.py
@@ -5,3 +5,15 @@
 
 # Your score is the sum of the points of the cards you have taken.
 # Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        current_score = sum(cardPoints[:k])
+        max_score = current_score
+        
+        for i in range(1, k + 1):
+            current_score += cardPoints[-i] - cardPoints[k - i]
+            max_score = max(max_score, current_score)
+        
+        return max_score
