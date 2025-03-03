bb87e76466450aaa70e620911a2724344f2c306b
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-03-03 21:06:26 +0530

    Update implement-queue-using-stacks.py

--- Day 14 - 03.03.25/implement-queue-using-stacks.py
+++ Day 14 - 03.03.25/implement-queue-using-stacks.py
@@ -1,6 +1,7 @@
 # STACKS and QUEUES - Easy
 
-# Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+# Implement a first in first out (FIFO) queue using only two stacks. 
+# The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
 
 # Implement the MyQueue class:
 # void push(int x) Pushes element x to the back of the queue.
