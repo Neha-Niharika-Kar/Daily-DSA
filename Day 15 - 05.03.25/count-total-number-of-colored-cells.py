23761c4f7ffdb979d919e00ab4cc5502dcc38ba0
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-03-05 23:31:55 +0530

    Update count-total-number-of-colored-cells.py

--- Day 15 - 05.03.25/count-total-number-of-colored-cells.py
+++ Day 15 - 05.03.25/count-total-number-of-colored-cells.py
@@ -1,4 +1,4 @@
-# MATH - Medium
+# MATHEMATICAL - Medium
 
 # There exists an infinitely large two-dimensional grid of uncolored unit cells. 
 # You are given a positive integer n, indicating that you must do the following routine for n minutes:
