36298e06bbe474dc0011db5f214f1cbf87de2d77
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-07-14 22:20:07 +0530

    Create convert-sorted-array-to-bst.py

---
+++ Day 30 - 14.07.25/convert-sorted-array-to-bst.py
@@ -0,0 +1,24 @@
+# TREES - Easy
+
+# Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def buildBST(left, right):
+            if left > right:
+                return None
+            
+            mid = (left + right) // 2
+            node = TreeNode(nums[mid])
+            node.left = buildBST(left, mid - 1)
+            node.right = buildBST(mid + 1, right)
+            return node
+
+        return buildBST(0, len(nums) - 1)
