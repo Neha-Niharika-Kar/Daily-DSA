38d13d0d52f49869832114fb6b14f80097789637
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-07-24 12:23:08 +0530

    Update word-search.py

--- Day 16 - 17.05.25/word-search.py
+++ Day 16 - 17.05.25/word-search.py
@@ -1,4 +1,4 @@
-# ARRAYS - Medium
+# MATRIX - Medium
 
 # Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 # The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. 
