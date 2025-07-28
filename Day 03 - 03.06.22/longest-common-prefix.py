a81eb07cc153bc96ec292ee48951c8fcf544d30e
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-07-28 21:12:16 +0530

    Update longest-common-prefix.py

--- Day 03 - 03.06.22/longest-common-prefix.py
+++ Day 03 - 03.06.22/longest-common-prefix.py
@@ -5,4 +5,14 @@
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        return os.path.commonprefix(strs)
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        
+        return prefix
