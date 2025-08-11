64645c13754a47836adcba155b7f5fe0220f3208
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-08-11 23:01:40 +0530

    Create string-to-integer-atoi.py

---
+++ Day 46 - 11.08.25/string-to-integer-atoi.py
@@ -0,0 +1,45 @@
+# STRINGS - Medium
+
+# Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+# The algorithm for myAtoi(string s) is as follows:
+
+# Whitespace: Ignore any leading whitespace (" ").
+# Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+# Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. 
+#             If no digits were read, then the result is 0.
+# Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. 
+#           Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+# Return the integer as the final result.
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+        
+        i = 0
+        n = len(s)
+        
+        while i < n and s[i] == ' ':
+            i += 1
+        
+        if i == n:
+            return 0
+    
+        sign = 1
+        if s[i] == '+':
+            i += 1
+        elif s[i] == '-':
+            sign = -1
+            i += 1
+        
+        num = 0
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+            
+            if num > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+            
+            num = num * 10 + digit
+            i += 1
+        
+        return sign * num
