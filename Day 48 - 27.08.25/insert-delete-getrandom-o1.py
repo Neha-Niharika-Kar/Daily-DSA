b694639b74a612c9ed3a267bf2fc3097d5f02206
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-08-27 12:10:27 +0530

    Create insert-delete-getrandom-o1.py

---
+++ Day 48 - 27.08.25/insert-delete-getrandom-o1.py
@@ -0,0 +1,45 @@
+# ARRAYS - Medium
+
+# Implement the RandomizedSet class:
+# RandomizedSet() Initializes the RandomizedSet object.
+# bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+# bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+# int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). 
+# Each element must have the same probability of being returned.
+# You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        self.arr = []
+        self.pos = {}
+
+    def insert(self, val: int) -> bool:
+        if val in self.pos:
+            return False
+        self.pos[val] = len(self.arr)
+        self.arr.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.pos:
+            return False
+        index = self.pos[val]
+        last_val = self.arr[-1]
+
+        self.arr[index] = last_val
+        self.pos[last_val] = index
+
+        self.arr.pop()
+        del self.pos[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
