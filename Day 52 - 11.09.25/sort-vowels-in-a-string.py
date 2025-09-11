31b4cb5198804e89750d3fd8802d05bf42498e2a
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-09-11 21:59:02 +0530

    Create sort-vowels-in-a-string.py

---
+++ Day 52 - 11.09.25/sort-vowels-in-a-string.py
@@ -0,0 +1,26 @@
+# STRINGS - Medium
+
+# Given a 0-indexed string s, permute s to get a new string t such that:
+# All consonants remain in their original places. 
+# More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+# The vowels must be sorted in the nondecreasing order of their ASCII values. 
+# More formally, for pairs of indices i, j with 0 <= i < j < s.length such that 
+# s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+# Return the resulting string.
+
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowels = 'aeiouAEIOU'
+        vowels_list = sorted([ch for ch in s if ch in vowels])
+        result = []
+        i = 0
+
+        for ch in s:
+            if ch in vowels:
+                result.append(vowels_list[i])
+                i += 1
+            else:
+                result.append(ch)
+        
+        return ''.join(result)
+      
