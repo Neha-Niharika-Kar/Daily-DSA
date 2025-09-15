4a87e08c37b50f7060dec78907b99777c2e9a76d
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-09-15 11:55:56 +0530

    Update maximum-number-of-words-you-can-type.py

--- Day 55 - 15.09.25/maximum-number-of-words-you-can-type.py
+++ Day 55 - 15.09.25/maximum-number-of-words-you-can-type.py
@@ -21,3 +21,15 @@ class Solution:
                 count += 1
         
         return count
+
+
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        broken = set(brokenLetters)
+        count = 0
+
+        for word in text.split():
+            if all(ch not in broken for ch in word):
+                count += 1
+
+        return count
