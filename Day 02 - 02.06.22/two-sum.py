841cce4e9719a97cc873df4f4fc3d04fdc5a7669
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-09-18 20:49:04 +0530

    Update two-sum.py

--- Day 02 - 02.06.22/two-sum.py
+++ Day 02 - 02.06.22/two-sum.py
@@ -12,3 +12,14 @@ class Solution:
                 y = nums[j]
                 if x + y == target:
                     return [i, j]
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+            
