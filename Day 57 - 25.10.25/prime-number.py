a6f7ad86b06882fd493d13e0f94ae6123f593bf6
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-10-25 10:53:45 +0530

    Create prime-number.py

---
+++ Day 57 - 25.10.25/prime-number.py
@@ -0,0 +1,13 @@
+# MATH - Easy
+
+# Given a number n, determine whether it is a prime number or not.
+
+class Solution:
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+        
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
