1aa6deb7efe74de4181e24ba2e4298fd918febbd
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-10-26 12:58:59 +0530

    Update longest-palindromic-substring.py

--- Day 17 - 26.05.25/longest-palindromic-substring.py
+++ Day 17 - 26.05.25/longest-palindromic-substring.py
@@ -2,26 +2,37 @@
 
 # Given a string s, return the longest palindromic substring in s.
 
+# Brute Force - O(n3)
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if not s or len(s) == 1:
-            return s
-        
-        start, end = 0, 0
+        n = len(s)
+        res = ""
+        for i in range(n):
+            for j in range(i, n):
+                substr = s[i:j+1]
+                if substr == substr[::-1] and len(substr) > len(res):
+                    res = substr
+        return res
 
-        def expand_around_center(left: int, right: int):
-            nonlocal start, end
+# Expand Around Center - O(n2)
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expandAroundCenter(left: int, right: int) -> str:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            
-            if right - left - 1 > end - start:
-                start = left + 1
-                end = right - 1
-        
+            return s[left+1:right]  # substring after mismatch
+
+        res = ""
         for i in range(len(s)):
-            expand_around_center(i,i)
-            expand_around_center(i,i+1)
+            odd = expandAroundCenter(i, i)      # Odd length palindrome
+            even = expandAroundCenter(i, i + 1)     # Even length palindrome
+
+            longer = odd if len(odd) > len(even) else even
+            if len(longer) > len(res):
+                res = longer
         
-        return s[start:end+1]
-      
+        return res
+
+
+# Manacher's Algorithm - O(n)
