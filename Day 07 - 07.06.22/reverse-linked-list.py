f96bf547822936d0bd9059ea64cc515a2fc13f00
Neha Niharika Kar <nehaniharikakar12@gmail.com>
2025-11-15 20:54:25 +0530

    Implement recursive linked list reversal
    Added a recursive method to reverse a linked list.

--- Day 07 - 07.06.22/reverse-linked-list.py
+++ Day 07 - 07.06.22/reverse-linked-list.py
@@ -17,3 +17,14 @@ class Solution:
             prev = curr
             curr = next_node
         return prev
+
+# Using recursion
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
